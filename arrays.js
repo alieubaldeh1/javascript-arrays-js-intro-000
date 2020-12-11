bb83f4f347de9790c104f25d1479b6cb802591d8
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 17:42:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,11 +9,11 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
 }
 
 function addElementToEndOfArray (array, element) {
-  // return array
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  // return array
+  return array.push(element);
 }
 
 function accessElementInArray (array, idx) {
