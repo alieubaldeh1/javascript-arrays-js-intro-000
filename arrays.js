778d531a2a6ea05ff9df15086e8a1b2d5af675c3
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 17:35:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,19 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray (array, element) {
   return array.unshift(element);
 }
+
+function destructivelyAddElementToBeginningOfArray (array, element) { 
+  // return array.
+}
+
+function addElementToEndOfArray (array, element) { 
+  // return array
+}
+
+function destructivelyAddElementToEndOfArray (array, element) { 
+  // return array
+}
+
+function accessElementInArray (array, idx) { 
+  return array[idx];
+}
