c3f98c0a8015fefdf6964ae8282a29b504e1da97
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 17:58:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.unshift(element);
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray (array, element) {
@@ -13,10 +14,12 @@ function addElementToEndOfArray (array, element) {
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  return array.push(element);
+  array.push(element);
+  return array 
 }
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-  return array.shift();
+  array.shift();
+  return shift
 }
 
 function removeElementFromBeginningOfArray(array) {
@@ -24,11 +27,12 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop();
+  array.pop();
+  return array 
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(-1);
+  return array.slice(0, array.length-1);
 }
 
 function accessElementInArray (array, idx) {
