ceabbb1b601d7d68e6f8b7cc839691277ba6dbde
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 17:04:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-addElementToBeginningOfArray (array, element) {
-  array.unshift(element)
-}
+// addElementToBeginningOfArray (array, element) {
+//   array.unshift(element)
+// }
